4b45131c680f5f94478f3ad3142e2fc4c4fe2371
Дмитрий Андреев <djandreev89@mail.ru>
2019-09-01 18:53:19 +1000

    шифр

--- шифрЦ/Program.cs
+++ шифрЦ/Program.cs
@@ -10,6 +10,41 @@ namespace шифрЦ
     {
         static void Main(string[] args)
         {
+
+            string user = Console.ReadLine();
+            Console.WriteLine(user);
+
+            Console.WriteLine(cipherC(user));
+
+            Console.ReadLine();
+
+        }
+        static string cipherC(string user)
+        {
+            string result = "";
+            for (int i = 0; i < user.Length; i++)
+            {
+                int userChar = user[i];
+
+                if (user[i] == ' ')
+                {
+                    result += " ";
+                    continue;
+                }
+                if (userChar > 87 && userChar < 91 || userChar > 119 && userChar < 123)
+                {
+                    result += Convert.ToChar(userChar - 23);
+                    continue;
+                }
+                if (userChar > 1100 && userChar < 1104 || userChar > 1068 && userChar < 1072)
+                {
+                    result += Convert.ToChar(userChar - 29);
+                    continue;
+
+                }
+                result += Convert.ToChar(userChar + 3);
+            }
+            return result;
         }
     }
 }
